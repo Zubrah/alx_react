d1fa75a077a54c572912bb2ee0c3eb0c18f360f3
Zubrah <z.msemo@alustudent.com>
2023-04-19 21:53:24 +0300

    alx

--- 0x00.Introduction to React/task_0/alx_dashboard/src/App.jsx
+++ 0x00.Introduction to React/task_0/alx_dashboard/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import styles from './App.module.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <div className={styles.container}>
+            <div className={styles.column1}></div>
+            <div className={styles.column2}>
+                <div className={styles.textContainer}>
+                    <div className={styles.centeredText}>
+                        <h1>Welcome to ALX SWE 😎</h1>
+                    </div>
+                    <div className={styles.centeredText}>
+                        <h2>We Learn and Develop By Doing!</h2>
+                    </div>
+                    <footer className={styles.footer}>
+                        <p> Copyright 2023 - ALX</p>
+                    </footer>
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
