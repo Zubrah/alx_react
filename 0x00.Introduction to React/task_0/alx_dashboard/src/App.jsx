8c1455f2f1987097d717135a532bbf795c9b593a
Zubrah <z.msemo@alustudent.com>
2023-04-20 13:17:29 +0300

    alx_final

--- 0x00.Introduction to React/task_0/alx_dashboard/src/App.jsx
+++ 0x00.Introduction to React/task_0/alx_dashboard/src/App.jsx
@@ -6,20 +6,29 @@ function App() {
         <div className={styles.container}>
             <div className={styles.column1}></div>
             <div className={styles.column2}>
+
                 <div className={styles.textContainer}>
                     <div className={styles.centeredText}>
-                        <h1>Welcome to ALX SWE 😎</h1>
-                    </div>
-                    <div className={styles.centeredText}>
-                        <h2>We Learn and Develop By Doing!</h2>
-                    </div>
-                    <footer className={styles.footer}>
-                        <p> Copyright 2023 - ALX</p>
-                    </footer>
+                        <h1>Welcome</h1>
+                        <h2>
+                            Let's explore and get involved in our community of learners where learn,discover,develop and grow together! 😎
 
+                        </h2>
+                        <div className={styles.btn_wrap}>
+                            <button className={styles.button}>I'm Interested!</button>
+
+                        </div>
+
+
+                    </div>
                 </div>
+                <footer className={styles.footer}>
+                    <p> Copyright 2023 - ALX</p>
+                </footer>
+
             </div>
         </div>
+
     );
 }
 
