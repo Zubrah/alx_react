42bbd41585c06020fa6ab7c4a75fcc1ea8f584bc
Zubrah <z.msemo@alustudent.com>
2023-04-27 10:15:04 +0300

    task_3

---
+++ 0x01.Learn React Components/task_3/alx_dashboard/src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styles from './App.module.css';
+//import { getFooterCopy, getFullYear } from './utils'
+import Notifications from "./Notifications/Notifications";
+//import logo from './assets/ALX+PNG.png';
+import Header from './Header/header';
+import Login from './Login/Login';
+import Footer from './Footer/Footer';
+
+function App() {
+    return (
+        <div className={styles.container}>
+            <div className={styles.column1}></div>
+            <div className={styles.column2}>
+
+                {/* Header Section */}
+                <Header />
+
+                {/* Body  Section */}
+                <Login />
+
+                {/* Notification Section Here!! */}
+                <Notifications />
+
+
+                {/* Footer Section */}
+                <Footer />
+
+            </div >
+        </div >
+    );
+}
+
+export default App;
