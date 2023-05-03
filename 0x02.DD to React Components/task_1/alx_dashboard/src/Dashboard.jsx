4db0ae1a5dcb8cb5d60e8330b332b7c8450a68bc
Zubrah <z.msemo@alustudent.com>
2023-05-03 19:23:14 +0300

    task_1

--- 0x02.DD to React Components/task_1/alx_dashboard/src/Dashboard.jsx
+++ 0x02.DD to React Components/task_1/alx_dashboard/src/Dashboard.jsx
@@ -42,6 +42,13 @@ class Dashboard extends Component {
         }
     }
 
+    // Create an array of courses
+    listCourses = [
+        { id: 1, name: 'ES6', credit: 60 },
+        { id: 2, name: 'Webpack', credit: 20 },
+        { id: 3, name: 'React', credit: 40 }
+    ];
+
     render() {
         const { isLoggedIn, displayDrawer } = this.props;
         return (
@@ -63,7 +70,8 @@ class Dashboard extends Component {
                     </div>
 
                     {/* Body  Section */}
-                    {isLoggedIn ? <CourseList /> : <Login />}
+                    {/* Modify to Show the list of courses */}
+                    {isLoggedIn ? <CourseList listCourses={this.listCourses} /> : <Login />}
 
                     {/* Footer Section */}
                     <Footer />
