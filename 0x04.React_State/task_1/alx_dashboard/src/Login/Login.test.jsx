b562df9a92953da7ddd75ce1dd0b6a45868099b4
Zubrah <z.msemo@alustudent.com>
2023-05-09 12:53:30 +0300

    task_1

--- 0x04.React_State/task_1/alx_dashboard/src/Login/Login.test.jsx
+++ 0x04.React_State/task_1/alx_dashboard/src/Login/Login.test.jsx
@@ -14,4 +14,27 @@ describe('<Login />', () => {
         expect(wrapper.find('input')).toHaveLength(2);
         expect(wrapper.find('label')).toHaveLength(2);
     });
+
+
+    it('should render without throwing an error', () => {
+        expect(wrapper.find('form.login').exists()).toBe(true);
+    });
+
+    it('should render a username input', () => {
+        expect(wrapper.find('input#username').length).toEqual(1);
+    });
+
+    it('should render a password input', () => {
+        expect(wrapper.find('input#password').length).toEqual(1);
+    });
+
+    it('should submit form with given values', () => {
+        const username = 'testuser';
+        const password = 'testpassword';
+        wrapper.find('input#username').simulate('change', { target: { value: username } });
+        wrapper.find('input#password').simulate('change', { target: { value: password } });
+        wrapper.find('form.login').simulate('submit', { preventDefault: () => { } });
+        expect(wrapper.find('input#username').prop('value')).toEqual(username);
+        expect(wrapper.find('input#password').prop('value')).toEqual(password);
+    });
 });
