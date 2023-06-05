d572aad6582bbb397996702058bf4a731490324c
Zubrah <z.msemo@alustudent.com>
2023-06-05 10:29:45 +0300

    task_5 Test update

--- 0x01.Learn React Components/task_5/alx_dashboard/src/Login/Login.test.jsx
+++ 0x01.Learn React Components/task_5/alx_dashboard/src/Login/Login.test.jsx
@@ -1,17 +1,44 @@
+// Test file (Login.test.jsx)
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render, fireEvent } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter
 import Login from './Login';
 
-// renders Login without crashing
-describe('<Login />', () => {
-    it('renders without crashing', () => {
-        shallow(<Login />);
+// Mock the CSS module
+jest.mock('./Login.module.css', () => ({
+    app_body: 'mocked-app-body',
+    inputContainer: 'mocked-input-container',
+    inputRow: 'mocked-input-row',
+    button: 'mocked-button',
+}));
+
+describe('Login', () => {
+    it('renders the login form', () => {
+        const { getByLabelText, getByText } = render(
+            <Router> {/* Add Router component wrapper */}
+                <Login />
+            </Router>
+        );
+        const emailInput = getByLabelText('Email Address:');
+        const passwordInput = getByLabelText('Password:');
+        const signInButton = getByText('Sign In');
+
+        expect(emailInput).toBeInTheDocument();
+        expect(passwordInput).toBeInTheDocument();
+        expect(signInButton).toBeInTheDocument();
     });
 
-    // Renders two inputs and labels 
-    it('renders two input and two label tags', () => {
-        const wrapper = shallow(<Login />);
-        expect(wrapper.find('input')).toHaveLength(2);
-        expect(wrapper.find('label')).toHaveLength(2);
+    it('triggers signIn function on button click', () => {
+        const navigateMock = jest.fn();
+        const { getByText } = render(
+            <Router> {/* Add Router component wrapper */}
+                <Login />
+            </Router>
+        );
+        const signInButton = getByText('Sign In');
+
+        fireEvent.click(signInButton); // Simulate button click
+
+        // Add your assertions for the signIn function being triggered
     });
 });
