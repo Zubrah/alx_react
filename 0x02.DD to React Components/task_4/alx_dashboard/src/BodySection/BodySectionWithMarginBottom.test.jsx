355372a77ffe43372c162e8c7dfbfa534ac02931
Zubrah <z.msemo@alustudent.com>
2023-06-08 12:37:20 +0300

    task_4 Test update

--- 0x02.DD to React Components/task_4/alx_dashboard/src/BodySection/BodySectionWithMarginBottom.test.jsx
+++ 0x02.DD to React Components/task_4/alx_dashboard/src/BodySection/BodySectionWithMarginBottom.test.jsx
@@ -1,35 +1,21 @@
-// Implement tests for BodySectionWithMarginBottom.jsx
 import React from 'react';
-import { shallow } from 'enzyme';
-import BodySection from '../BodySection/BodySection';
+import { render, screen } from '@testing-library/react';
 import BodySectionWithMarginBottom from './BodySectionWithMarginBottom';
 
-describe('<BodySectionWithMarginBottom />', () => {
-    it('renders a div with the class bodySectionWithMargin', () => {
-        const wrapper = shallow(
-            <BodySectionWithMarginBottom title="Test">
-                <p>Some text</p>
-            </BodySectionWithMarginBottom>
-        );
-        expect(wrapper.find('div.bodySectionWithMargin')).toHaveLength(1);
-    });
+// Mock the CSS module
+jest.mock('./BodySection.module.css', () => ({
+    bodySectionWithMargin: 'mock-bodySectionWithMargin',
+}));
 
-    it('renders a BodySection component with the same props', () => {
-        const wrapper = shallow(
-            <BodySectionWithMarginBottom title="Test">
-                <p>Some text</p>
+describe('BodySectionWithMarginBottom', () => {
+    test('renders BodySectionWithMarginBottom component with title and children', () => {
+        render(
+            <BodySectionWithMarginBottom title="Test Title">
+                <p>Test Content</p>
             </BodySectionWithMarginBottom>
         );
-        expect(wrapper.find(BodySection)).toHaveLength(1);
-        expect(wrapper.find(BodySection).props()).toEqual({ title: 'Test', children: <p>Some text</p> });
-    });
-
-    it('applies a margin bottom of 40px', () => {
-        const wrapper = shallow(
-            <BodySectionWithMarginBottom title="Test">
-                <p>Some text</p>
-            </BodySectionWithMarginBottom>
-        );
-        expect(wrapper.find('div.bodySectionWithMargin').prop('style')).toEqual({ marginBottom: '40px' });
+        // Add assertions for the rendered components and their content
+        expect(screen.getByText('Test Title')).toBeInTheDocument();
+        expect(screen.getByText('Test Content')).toBeInTheDocument();
     });
 });
--- 0x02.DD to React Components/task_4/alx_dashboard/src/Dashboard.test.jsx
+++ 0x02.DD to React Components/task_4/alx_dashboard/src/Dashboard.test.jsx
@@ -1,40 +1,49 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 import Dashboard from './Dashboard';
 
+// Mock the necessary components
+jest.mock('./Notifications/Notifications', () => () => <div>Mock Notifications</div>);
+jest.mock('./CourseList/CourseList', () => ({ listCourses }) => (
+    <div>
+        Mock CourseList
+        {listCourses.map((course) => (
+            <div key={course.id}>{course.name}</div>
+        ))}
+    </div>
+));
+jest.mock('../Login/Login', () => () => <div>Mock Login</div>);
+jest.mock('../Footer/Footer', () => () => <div>Mock Footer</div>);
+jest.mock('../BodySection/BodySection', () => ({ title, children }) => (
+    <div>
+        Mock BodySection
+        <h2>{title}</h2>
+        {children}
+    </div>
+));
+jest.mock('../BodySection/BodySectionWithMarginBottom', () => ({ title, children }) => (
+    <div>
+        Mock BodySectionWithMarginBottom
+        <h2>{title}</h2>
+        {children}
+    </div>
+));
 
-
-//Implement test for Dashboard.jsx file
 describe('Dashboard', () => {
-    it('renders without crashing', () => {
-        shallow(<Dashboard />);
+    test('renders Dashboard component with all sections', () => {
+        render(<Dashboard />);
+        // Add assertions for the rendered components and their content
+        expect(screen.getByText('Mock Notifications')).toBeInTheDocument();
+        expect(screen.getByText('Mock CourseList')).toBeInTheDocument();
+        expect(screen.getByText('ES6')).toBeInTheDocument();
+        expect(screen.getByText('Webpack')).toBeInTheDocument();
+        expect(screen.getByText('React')).toBeInTheDocument();
+        expect(screen.getByText('Mock Login')).toBeInTheDocument();
+        expect(screen.getByText('Mock Footer')).toBeInTheDocument();
+        expect(screen.getByText('Mock BodySection')).toBeInTheDocument();
+        expect(screen.getByText('Course list')).toBeInTheDocument();
+        expect(screen.getByText('Mock BodySectionWithMarginBottom')).toBeInTheDocument();
+        expect(screen.getByText('Log in Access Full Dashboard')).toBeInTheDocument();
+        expect(screen.getByText('News from the School')).toBeInTheDocument();
     });
-
-    //renders header and footer 
-    it('renders a header and a footer', () => {
-        const wrapper = shallow(<Dashboard />);
-        expect(wrapper.find('Header')).toHaveLength(1);
-        expect(wrapper.find('Footer')).toHaveLength(1);
-    });
-
-    it('renders three sections', () => {
-        const wrapper = shallow(<Dashboard />);
-        expect(wrapper.find('div.App-body')).toHaveLength(1);
-        expect(wrapper.find('div.App-body div.App-body-content')).toHaveLength(1);
-        expect(wrapper.find('div.App-body div.App-body-content div.App-body-content-school-info')).toHaveLength(1);
-        expect(wrapper.find('div.App-body div.App-body-content div.App-body-content-courses')).toHaveLength(1);
-    });
-
-    // It calls a logOut functions and shows the alert
-    it('calls logOut function and shows alert when Ctrl+h are pressed', () => {
-        const logOutMock = jest.fn();
-        const alertMock = jest.spyOn(window, 'alert').mockImplementation(() => { });
-        const wrapper = shallow(<Dashboard logOut={logOutMock} />);
-        wrapper.instance().handleKeyDown({ ctrlKey: true, key: 'h' });
-        expect(alertMock).toHaveBeenCalledWith('Logging you out');
-        expect(logOutMock).toHaveBeenCalled();
-        alertMock.mockRestore();
-    });
-}
-
-);
+});
