bf7d9f4424221a9492a3fe9575526de5da346420
Zubrah <z.msemo@alustudent.com>
2023-06-08 14:07:02 +0300

    task_5 Test update

--- 0x02.DD to React Components/task_5/alx_dashboard/src/Dashboard.test.jsx
+++ 0x02.DD to React Components/task_5/alx_dashboard/src/Dashboard.test.jsx
@@ -1,40 +1,72 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 import Dashboard from './Dashboard';
 
+// Mock the necessary components
+jest.mock('./Notifications/Notifications.jsx', () => () => <div>Mock Notifications</div>);
+jest.mock('./CourseList/CourseList.jsx', () => ({ listCourses }) => (
+    <div>
+        Mock CourseList
+        {listCourses.map((course) => (
+            <div key={course.id}>{course.name}</div>
+        ))}
+    </div>
+));
+jest.mock('./Login/Login.jsx', () => () => <div>Mock Login</div>);
+jest.mock('./Footer/Footer.jsx', () => () => <div>Mock Footer</div>);
+jest.mock('./BodySection/BodySection.jsx', () => ({ title, children }) => (
+    <div>
+        Mock BodySection
+        <h2>{title}</h2>
+        {children}
+    </div>
+));
+jest.mock('./BodySection/BodySectionWithMarginBottom.jsx', () => ({ title, children }) => (
+    <div>
+        Mock BodySectionWithMarginBottom
+        <h2>{title}</h2>
+        {children}
+    </div>
+));
+jest.mock('./HOC/WithLogging.jsx', () => (Component) => (props) => (
+    <div>
+        Mock WithLogging
+        <Component {...props} />
+    </div>
+));
 
-
-//Implement test for Dashboard.jsx file
 describe('Dashboard', () => {
-    it('renders without crashing', () => {
-        shallow(<Dashboard />);
+    test('renders Dashboard component with all sections when logged in', () => {
+        render(<Dashboard />);
+        // Add assertions for the rendered components and their content
+        expect(screen.getByText('Mock Notifications')).toBeInTheDocument();
+        expect(screen.getByText('Mock CourseList')).toBeInTheDocument();
+        expect(screen.getByText('ES6')).toBeInTheDocument();
+        expect(screen.getByText('Webpack')).toBeInTheDocument();
+        expect(screen.getByText('React')).toBeInTheDocument();
+        expect(screen.queryByText('Mock Login')).not.toBeInTheDocument();
+        expect(screen.getByText('Mock Footer')).toBeInTheDocument();
+        expect(screen.getByText('Mock BodySection')).toBeInTheDocument();
+        expect(screen.getByText('Course list')).toBeInTheDocument();
+        expect(screen.getByText('Mock BodySectionWithMarginBottom')).toBeInTheDocument();
+        expect(screen.queryByText('Log in Access Full Dashboard')).not.toBeInTheDocument();
+        expect(screen.getByText('News from the School')).toBeInTheDocument();
     });
 
-    //renders header and footer 
-    it('renders a header and a footer', () => {
-        const wrapper = shallow(<Dashboard />);
-        expect(wrapper.find('Header')).toHaveLength(1);
-        expect(wrapper.find('Footer')).toHaveLength(1);
+    test('renders Dashboard component with all sections when logged out', () => {
+        render(<Dashboard isLoggedIn={false} />);
+        // Add assertions for the rendered components and their content
+        expect(screen.getByText('Mock Notifications')).toBeInTheDocument();
+        expect(screen.queryByText('Mock CourseList')).not.toBeInTheDocument();
+        expect(screen.queryByText('ES6')).not.toBeInTheDocument();
+        expect(screen.queryByText('Webpack')).not.toBeInTheDocument();
+        expect(screen.queryByText('React')).not.toBeInTheDocument();
+        expect(screen.getByText('Mock Login')).toBeInTheDocument();
+        expect(screen.getByText('Mock Footer')).toBeInTheDocument();
+        expect(screen.getByText('Mock BodySection')).toBeInTheDocument();
+        expect(screen.queryByText('Course list')).not.toBeInTheDocument();
+        expect(screen.getByText('Mock BodySectionWithMarginBottom')).toBeInTheDocument();
+        expect(screen.getByText('Log in Access Full Dashboard')).toBeInTheDocument();
+        expect(screen.getByText('News from the School')).toBeInTheDocument();
     });
-
-    it('renders three sections', () => {
-        const wrapper = shallow(<Dashboard />);
-        expect(wrapper.find('div.App-body')).toHaveLength(1);
-        expect(wrapper.find('div.App-body div.App-body-content')).toHaveLength(1);
-        expect(wrapper.find('div.App-body div.App-body-content div.App-body-content-school-info')).toHaveLength(1);
-        expect(wrapper.find('div.App-body div.App-body-content div.App-body-content-courses')).toHaveLength(1);
-    });
-
-    // It calls a logOut functions and shows the alert
-    it('calls logOut function and shows alert when Ctrl+h are pressed', () => {
-        const logOutMock = jest.fn();
-        const alertMock = jest.spyOn(window, 'alert').mockImplementation(() => { });
-        const wrapper = shallow(<Dashboard logOut={logOutMock} />);
-        wrapper.instance().handleKeyDown({ ctrlKey: true, key: 'h' });
-        expect(alertMock).toHaveBeenCalledWith('Logging you out');
-        expect(logOutMock).toHaveBeenCalled();
-        alertMock.mockRestore();
-    });
-}
-
-);
+});
--- 0x02.DD to React Components/task_5/alx_dashboard/src/utils.test.jsx
+++ 0x02.DD to React Components/task_5/alx_dashboard/src/utils.test.jsx
@@ -1,31 +1,27 @@
 import { getFullYear, getFooterCopy, getLatestNotification } from './utils';
 
-
-
-// Test for getFullYear
 describe('getFullYear', () => {
-    it('returns the correct year', () => {
+    it('should return the current year', () => {
         const currentYear = new Date().getFullYear();
         expect(getFullYear()).toBe(currentYear);
     });
 });
 
-
-// Test for get Footer for Indexes
 describe('getFooterCopy', () => {
-    it('returns "ALX SWE" when isIndex is true', () => {
-        expect(getFooterCopy(true)).toBe('ALX SWE');
+    it('should return "ALX SWE" if isIndex is true', () => {
+        const isIndex = true;
+        expect(getFooterCopy(isIndex)).toBe('ALX SWE');
     });
 
-    it('returns "ALX Main dashboard" when isIndex is false', () => {
-        expect(getFooterCopy(false)).toBe('ALX Main dashboard');
+    it('should return "ALX Main dashboard" if isIndex is false', () => {
+        const isIndex = false;
+        expect(getFooterCopy(isIndex)).toBe('ALX Main dashboard');
     });
 });
 
-
-// Test for getLatestNotification
 describe('getLatestNotification', () => {
-    it('returns the correct string', () => {
-        expect(getLatestNotification()).toBe('<strong>Urgent requirement</strong> - complete by EOD');
+    it('should return the latest notification in the correct format', () => {
+        const expectedNotification = '<strong>Urgent requirement</strong> - complete by EOD';
+        expect(getLatestNotification()).toBe(expectedNotification);
     });
 });
